a2692c8c604fa03bafa6a15f993d42cdf37095cd
Clement TONI <clement.toni@epitech.eu>
2022-04-29 12:22:12 +0200

    add : constructor

---
+++ machine/src/function_cpu.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2021
+** corewar
+** File description:
+** function_cpu.c
+*/
+
+#include "my.h"
+#include "op.h"
+
+void live(int *option)
+{
+    my_putstr("The player ");
+    my_put_nbr((int)option[0]);
+    my_putstr(" is alive..\n");
+}
+
+void ld_func(int *option)
+{
+
+}
