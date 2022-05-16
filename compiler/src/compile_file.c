69f57bf5e1b2c02a2c2e5037c762e68f344b736e
Gaby <gabriel.lepinay@epitech.eu>
2022-05-16 10:38:36 +0200

    fix: compile_file.c

--- compiler/src/compile_file.c
+++ compiler/src/compile_file.c
@@ -39,42 +39,31 @@ static char *get_exec_name(char *filename)
     return exec_name;
 }
 
-// int convert_line(char **line, exec_t *ex)
-// {
-//     char *tmp = my_strdup(*line);
-//     int index = write_instruct(line, ex);
-
-//     if (index == -2) {
-//         declare_label(&tmp, ex);
-//         return continue_writting_line(line, ex);
-//     }
-//     free(tmp);
-//     if (index < 0 || write_parameters(line, ex, index))
-//         return -1;
-//     return 0;
-// }
-
-static int write_executable(char *filename, char *exec_file)
+static int write_line_executable(FILE *src, exec_t *ex)
 {
     size_t len = 0;
     char *line = NULL;
-    FILE *src = fopen(filename, "r");
-    exec_t *ex = new_exec();
-    header_t *header = get_header(src); //TODO: KEEP THIS LINE FOR LATER, WORKS FINE
 
-    if (!src)
-        return 84;
     while (getline(&line, &len, src) != -1) {
         if (*line == '\n' || *line == COMMENT_CHAR || *line == '\0')
             continue;
         remove_final_newline(line);
         line[len - 1]  = '\0';
-        //process_this_line
-        if (process_line(ex, &line)) {
-
-        }
+        process_line(ex, &line);
         len = 0;
     }
+    return (0);
+}
+
+static int write_executable(char *filename, char *exec_file)
+{
+    FILE *src = fopen(filename, "r");
+    exec_t *ex = new_exec();
+    header_t *header = get_header(src);
+
+    if (!src)
+        return 84;
+    write_line_executable(src, ex);
     fclose(src);
     write_exec_binary(ex);
     output_binary_to_file(exec_file, ex, header);
