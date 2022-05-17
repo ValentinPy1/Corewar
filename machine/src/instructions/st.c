7f8b7ef389857619b07f47dcf8c41c8a41562c36
Clement TONI <clement.toni@epitech.eu>
2022-05-17 17:42:48 +0200

    backup before change functions vm

--- machine/src/instructions/st.c
+++ machine/src/instructions/st.c
@@ -10,18 +10,18 @@
 
 void st_func(vm_t *vm, process_t *process, ope_t *ope)
 {
-    int reg_index = ope->args[0];
-    void *data = (ope->size_type[1] == T_REG) ? &process->reg[ope->args[1]]
-    : vm->ram->mem + ope->args[1] % IDX_MOD;
+    int reg_index = ope->real_args[0];
+    void *data = (ope->size_type[1] == T_REG) ? &process->reg[ope->real_args[1]]
+    : vm->ram->mem + ope->real_args[1] % IDX_MOD;
     load_data_from_reg(&process->reg[reg_index], data, REG_SIZE);
 }
 
 void sti_func(vm_t *vm, process_t *process, ope_t *ope)
 {
-    int reg_index = ope->args[0];
-    int adress = process->pc + ope->args[1] % IDX_MOD;
-    void *data = (ope->size_type[1] == T_REG) ? &process->reg[ope->args[1]]
-    : vm->ram->mem + ope->args[1] % IDX_MOD;
+    int reg_index = ope->real_args[0];
+    int adress = process->pc + ope->real_args[1] % IDX_MOD;
+    void *data = (ope->size_type[1] == T_REG) ? &process->reg[ope->real_args[1]]
+    : vm->ram->mem + ope->real_args[1] % IDX_MOD;
     load_data_from_reg(&process->reg[reg_index], data, REG_SIZE);
     load_data_in_reg(&process->reg[reg_index], vm->ram->mem + adress, REG_SIZE);
 }
