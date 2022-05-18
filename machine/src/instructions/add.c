063142405500e1f87a857597d9cc459a73c19309
Clement TONI <clement.toni@epitech.eu>
2022-05-18 17:31:35 +0200

    fix: add and sub func

--- machine/src/instructions/add.c
+++ machine/src/instructions/add.c
@@ -7,6 +7,7 @@
 
 #include <stdlib.h>
 #include "machine.h"
+#include "asm.h"
 
 static void add_from_registers(int *r1, int *r2, int *r3, process_t *process)
 {
@@ -31,7 +32,10 @@ void add_func(vm_t *vm, process_t *process, ope_t *ope)
     }
     load_data_from_reg(&process->reg[ope->args[0]], r1, REG_SIZE);
     load_data_from_reg(&process->reg[ope->args[0]], r2, REG_SIZE);
+    invert_endianess(&r1, sizeof(int));
+    invert_endianess(&r2, sizeof(int));
     add_from_registers(r1, r2, r3, process);
+    invert_endianess(&r3, sizeof(int));
     load_data_in_reg(&process->reg[ope->args[2]], r3, REG_SIZE);
     free(r1);
     free(r2);
