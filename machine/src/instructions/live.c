2b2501346f2b604f209222702fd1730236e0f56f
Clement TONI <clement.toni@epitech.eu>
2022-05-19 10:57:00 +0200

    fix: merge

--- machine/src/instructions/live.c
+++ machine/src/instructions/live.c
@@ -11,11 +11,11 @@
 void live_func(vm_t *vm, process_t *process, ope_t *ope)
 {
     my_putstr("The player ");
-    my_put_nbr(ope->real_args[0]);
+    my_put_nbr(ope->real_args[1]);
     my_putstr(" (");
     my_putstr(process->name);
     my_putstr(") is alive..\n");
-    process->player_id_alive = ope->real_args[0];
+    process->player_id_alive = ope->real_args[1];
     process->last_live = vm->cycle;
     vm->players[ope->real_args[1]].last_live = vm->cycle;
 }
